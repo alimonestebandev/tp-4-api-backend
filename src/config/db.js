f9f70d02fb5a8e4ffbee148b544fa64a2631f327
alimon esteban <estebanfacundoalimon@gmail.com>
2025-10-31 01:16:21 -0300

    configuracion de database con variables de entorno

---
+++ src/config/db.js
@@ -0,0 +1,12 @@
+const { Sequelize } = require('sequelize');
+const variables = require('../utils/variables.js');
+
+const sequelize = new Sequelize(variables.DB_NAME, variables.DB_USER, variables.DB_PASS, {
+host: variables.DB_HOST,
+port: variables.DB_PORT || 3306,
+dialect: 'mysql',
+logging: false
+});
+
+
+module.exports = sequelize;
