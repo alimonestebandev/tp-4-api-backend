8e74604b45d2b9261d34360e452a5b0ce44e4ed5
alimon esteban <estebanfacundoalimon@gmail.com>
2025-10-31 04:10:09 -0300

    arreglo

--- src/config/db.js
+++ src/config/db.js
@@ -1,7 +1,8 @@
 const { Sequelize } = require('sequelize');
 const variables = require('../utils/variables.js');
 
-const sequelize = new Sequelize(variables.DB_NAME, variables.DB_USER, variables.DB_PASS, {
+
+const sequelize = new Sequelize(variables.DB_NAME, variables.DB_USER, variables.DB_PASS , {
 host: variables.DB_HOST,
 port: variables.DB_PORT || 3306,
 dialect: 'mysql',
