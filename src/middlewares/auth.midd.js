9d697257ed5f3f0275561a57d543d1cc24942e25
alimon esteban <estebanfacundoalimon@gmail.com>
2025-10-31 04:13:27 -0300

    verificacion nueva de token por medio de cookies

--- src/middlewares/auth.midd.js
+++ src/middlewares/auth.midd.js
@@ -2,18 +2,23 @@ const { verifyToken } = require('../utils/jwt.js');
 
 
 const authMiddleware = (req, res, next) => {
-try {
-const token = req.body.token;
-// if (!token) return res.status(401).json({ message: 'No autorizado: falta token' });
 
+   try {
+    const token = req.cookies?.token;
 
-// const parts = token.split(' ');
-// if (parts.length !== 2 || parts[0] !== 'Bearer') return res.status(401).json({ message: 'Token inválido' });
+    if (!token) {
+      return res.status(401).json({ message: 'No autorizado: falta token' });
+    }
 
+    const payload = verifyToken(token);
 
-const payload = verifyToken(token);
-req.userId = payload.id;
-next();
+    if (!payload || !payload.id) {
+      return res.status(401).json({ message: 'Token inválido' });
+    }
+
+    req.userId = payload.id;
+    next();
+
 } catch (err) {
 return res.status(401).json({ message: 'Token inválido o expirado' });
 }
